2bd42f4507afa048033bd62da84dc8bd890cb919
Simon Weatherley <sweatherley67@gmail.com>
2022-01-16 20:13:08 +0000

    practice with VSCode

--- src/countDuplicateCharacters.java
+++ src/countDuplicateCharacters.java
@@ -1,6 +1,22 @@
+
 public class countDuplicateCharacters {
-    public static void main(String[] args) {
-        
-    }        
+
+    import Map;
+    /*
+     * This file counts the duplicate characters in a string
+     * 
+     * It iterates through the characters in the string and stores each character in
+     * a map where the character is the key with an integer value representing the
+     * count.
+     * 
+     * if the character doesn't exist in the map, then it is added to the map with
+     * an integer value of 1
+     * 
+     * if the character already exists, then the value is incremented by 1
+     */
     }
-}
+    public static void main(String[] args){
+        countDuplicateCharacters charCounter = new countDuplicateCharacters();
+    } 
+ 
+
