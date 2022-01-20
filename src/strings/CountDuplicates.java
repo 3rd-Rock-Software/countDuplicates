e5f302b5a485b5e94e84822158fa13068a916aa1
Simon Weatherley <sweatherley67@gmail.com>
2022-01-20 20:11:19 +0000

    working on getting least and most used character

--- src/strings/CountDuplicates.java
+++ src/strings/CountDuplicates.java
@@ -2,12 +2,9 @@ package strings;
 
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.Map.Entry;
 
 public class CountDuplicates {
-    char minCH;
-    char maxCH;
-    int minValue = 0;
-    int maxValue = 0;
 
     /*
      * iterate the characters of a string and store them in a map
@@ -19,65 +16,10 @@ public class CountDuplicates {
      * count by 1
      * otherwise add the character to the map with a value of 1
      */
-    /**
-     * @return the minValue
-     */
-    public int getMinValue() {
-        return minValue;
-    }
-
-    /**
-     * @param minValue the minValue to set
-     */
-    public void setMinValue(int minValue) {
-        this.minValue = minValue;
-    }
-
-    /**
-     * @return the maxValue
-     */
-    public int getMaxValue() {
-        return maxValue;
-    }
-
-    /**
-     * @param maxValue the maxValue to set
-     */
-    public void setMaxValue(int maxValue) {
-        this.maxValue = maxValue;
-    }
-
-    /**
-     * @return the minCH
-     */
-    public char getMinCH() {
-        return minCH;
-    }
-
-    /**
-     * @param minCH the minCH to set
-     */
-    public void setMinCH(char minCH) {
-        this.minCH = minCH;
-    }
-
-    /**
-     * @return the maxCH
-     */
-    public char getMaxCH() {
-        return maxCH;
-    }
-
-    /**
-     * @param maxCH the maxCH to set
-     */
-    public void setMaxCH(char maxCH) {
-        this.maxCH = maxCH;
-    }
 
     public void countDuplicates(String str) {
         String lowerCase = str.toLowerCase();
-        Map<Character, Integer> results = new TreeMap<>();
+        TreeMap<Character, Integer> results = new TreeMap<>();
         for (char ch : lowerCase.toCharArray()) {
             if (results.containsKey(ch)) {
                 results.put(ch, results.get(ch) + 1);
@@ -85,37 +27,12 @@ public class CountDuplicates {
                 results.put(ch, 1);
             }
         }
-        getMinValue(results);
         System.out.println("sorted by key:");
         System.out.println();
         System.out.println(results);
         System.out.println();
-        System.out.println("The least used character is : " + getMinCH());
         // System.out.println("sorted by value:");
         // System.out.println(sortMapByValues(results));
-    }
-
-    private void getMinValue(Map<Character, Integer> results) {
-
-        // int maxValue = 0;
-
-        // there is a bug in the logic for the if statement here. on the initial
-        // iteration getminvalue = 0
-
-        // the logic needs to:
-
-        // initialise min value with the first entry value
-        // if minvalue is more than the next entry value, minValue = entry value
-        for (Map.Entry<Character, Integer> entry : results.entrySet()) {
-            if (getMinValue() < entry.getValue()) {
-                setMinValue(entry.getValue());
-                setMinCH(entry.getKey());
-            }
-            // if (entry.getValue() > maxValue){
-            // maxValue = entry.getValue();
-            // maxCH = entry.getKey();
-            // }
-        }
     }
 
     // private Map sortMapByValues(Map<Character, Integer> results)
